00c85b4ef618accdfcd158b7ec06d2d7d0349a9c
Phil2711 <philippemoenaert@me.com>
2022-02-01 16:28:29 +0100

    corrections des fonctions python

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import plotly.graph_objects as go
+# import plotly.graph_objects as go
 import xgboost as xgb
 
 # Calcule le résultat du candidat selon le modèle choisi
@@ -32,31 +32,31 @@ def risque_client(X,
 #  -valeur : le résultat du candidat pour le modèle
 # En sortie :
 #  -une jauge positionnant le candidat par raport au seuil
-def trace_jauge(seuil,
-                valeur) :
+# def trace_jauge(seuil,
+                # valeur) :
 
-    la_jauge =  go.Figure(go.Indicator(domain = {'x': [0, 0.5], 
-                                                'y': [0, 0.]},
-                                                value = valeur,
-                                                    mode = 'gauge+number+delta',
-                                                    title = {'text': "Seuil"},
-                                                    delta = {'reference': seuil},
-                                                    gauge = {'axis': {'range': [None, 
-                                                                                1]},
-                                                            'steps' : [
-                                                                {'range': [0,
-                                                                            0.3],
-                                                                'color': 'lightgreen'},
-                                                                {'range': [0.3,
-                                                                            1],
-                                                                'color': 'orange'}],
-                                                            'threshold' : {'line': {'color': 'red',
-                                                                                    'width': 4},
-                                                                                    'thickness': 0.75,
-                                                                                    'value': valeur}}))
+    # la_jauge =  go.Figure(go.Indicator(domain = {'x': [0, 0.5], 
+                                                # 'y': [0, 0.]},
+                                                # value = valeur,
+                                                    # mode = 'gauge+number+delta',
+                                                    # title = {'text': "Seuil"},
+                                                    # delta = {'reference': seuil},
+                                                    # gauge = {'axis': {'range': [None, 
+                                                                                # 1]},
+                                                            # 'steps' : [
+                                                                # {'range': [0,
+                                                                            # 0.3],
+                                                                # 'color': 'lightgreen'},
+                                                                # {'range': [0.3,
+                                                                            # 1],
+                                                                # 'color': 'orange'}],
+                                                            # 'threshold' : {'line': {'color': 'red',
+                                                                                    # 'width': 4},
+                                                                                    # 'thickness': 0.75,
+                                                                                    # 'value': valeur}}))
 
-    la_jauge.to_html()
+    # la_jauge.to_html()
 
-def test(x) :
+# def test(x) :
     
-    return x*2
+    # return x*2
