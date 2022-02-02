8b963bade34beb9f51f5aa5c74dad0aae76b42e5
Phil2711 <philippemoenaert@me.com>
2022-02-02 13:33:44 +0100

    corrections

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def anciennetés_clients():
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     # app.run(host='127.0.0.1', debug = True)
-   app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), threaded=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), threaded=True)
