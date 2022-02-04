47a76089bc31162f1cd0badad50d8b9f5fa71ff2
Phil2711 <philippemoenaert@me.com>
2022-02-04 08:58:16 +0100

    changement

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ app = Flask(__name__)
 # chemin_données = r'https://github.com/Phil2711/P7_OCR/tree/bases/Data/'
 # chemin = r'C:\Users\Sdis59\Documents\OpenClassroom\P7_DS_OCR'
 X_SMOTE = pd.read_csv('X_SMOTE.csv')
-les_clients = pd.read_csv('les_clients.csv', header = 0)
 # X_SMOTE = pd.read_csv(chemin_données + 'X_SMOTE.csv')
 
 # @app.route('/mise_à_jour', methods=['POST'])
@@ -55,16 +54,17 @@ def calcul_du_risque() :
 
     # return la_jauge
     
-@app.route('/')
+@app.route('/', methods = [GET])
 def racine():
     
     # os.system('kaggle competitions download -p \'Data\\\' -c \'home-credit-default-risk\'')
     
+    les_clients = pd.read_csv('les_clients.csv', header = 0)
 
     # les_clients.columns = ['id_client']
     
-    return render_template('tdb.html', clients = range(100002, 111683, 1) )
-                           # clients = les_clients['SK_ID_CURR'].unique().tolist())
+    return render_template('tdb.html',
+                           clients = les_clients['SK_ID_CURR'].unique().tolist())
         
  
 #================================================================================
