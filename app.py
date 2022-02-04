2b5c8e0d6b9fd74824bd2cee5efe98ecce985e11
Phil2711 <philippemoenaert@me.com>
2022-02-04 14:47:50 +0100

    changement

--- app.py
+++ app.py
@@ -10,23 +10,7 @@ from functions import risque_client
 
 app = Flask(__name__)
 
-URL = 'https://1drv.ms/f/s!Am80QXETynO-hbkQa3ev8H-WWAcDtA'
-
-# chemin_données = r'https://github.com/Phil2711/P7_OCR/tree/bases/Data/'
-# chemin = r'C:\Users\Sdis59\Documents\OpenClassroom\P7_DS_OCR'
 X_SMOTE = pd.read_csv('X_SMOTE.csv')
-# X_SMOTE = pd.read_csv(chemin_données + 'X_SMOTE.csv')
-
-# @app.route('/mise_à_jour', methods=['POST'])
-# def webhook():
-        # if request.method == 'POST':
-            # repo = git.Repo('https://github.com/Phil2711/P7_OCR')
-            # origin = repo.remotes.p7
-            
-# origin.pull()
-# return 'Mise à jour réussie', 200
-        # else:
-            # return 'Erreur', 400
             
 #================================================================================
     
@@ -46,25 +30,15 @@ def calcul_du_risque() :
                         'classe': la_classe
                         }
                     })
-    
-# @app.route('/functions/jauge/', methods = ['GET'])
-# def jauge() :
 
-    # risque = float(request.args.get('risque', 0))
-
-    # la_jauge = trace_jauge(le_seuil, risque)
-
-    # return la_jauge
     
 @app.route('/')
 def racine():
     
-    # os.system('kaggle competitions download -p \'Data\\\' -c \'home-credit-default-risk\'')
-    
+  
     les_clients = pd.read_csv('les_clients.csv',
                                sep = ',')
-    # clients = [i for in in range(100002, 111683, 1)]
-    # les_clients['SK_ID_CURR'].unique().tolist()
+
     
     return render_template('tdb.html',
                            clients = les_clients['SK_ID_CURR'].unique().tolist())
@@ -87,6 +61,6 @@ def anciennetés_clients():
 #=====================================main=======================================
 
 if __name__ == "__main__":
-#    app.config['TEMPLATES_AUTO_RELOAD'] = True
+
     # app.run(host='127.0.0.1', debug = True)
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), threaded=True, debug = True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 3002)), threaded=True, debug = True)
