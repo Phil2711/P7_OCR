c2f1117cf6037c9a8aa42d2238587ddb52c815e7
Phil2711 <81799909+Phil2711@users.noreply.github.com>
2022-02-16 13:55:44 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,9 +27,7 @@ if X_SMOTE.columns[0] == 'Unnamed: 0' :
     axis = 1,
     inplace = True)
 # illustrateur_shap = shap.TreeExplainer(le_modèle_ajusté, X_SMOTE, y = 192)
-nom_illustrateur = 'illustrateur.save'
-pickle.dump(explainer, open(nom_illustrateur, 'wb'))
-illustrateur_shap = pickle.load(open(nom_illustrateur, 'rb'))
+illustrateur_shap = pickle.load(open('illustrateur.save', 'rb'))
 #with open('illustrateur.save', 'rb') as fichier_illustrateur :
  #   illustrateur_shap = pickle.load(fichier_illustrateur)
 with open('valeurs_shap.save', 'rb') as fichier_shap :
